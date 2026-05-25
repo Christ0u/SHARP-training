c0b5ced0bd30a402055e5fdde83c73f5a663710e
Christopher GERARD <christopher.gerard@ikmail.com>
2026-05-25 13:00:04 +0200

    CHG : Amélioration de la pipeline d'entraînement
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- main.py
+++ main.py
@@ -1,40 +1,84 @@
+import random
 from ultralytics import YOLO
 
 def main():
-    model = YOLO('yolo26n.pt')
+    model = YOLO('yolo26s.pt')
+    seed = random.randint(0, 9999)
 
     model.train(
         data=r'data.yaml',
-        epochs=350,
-        imgsz=640,
-        batch=8, # Pour 4070, 8 pour 3060
+        epochs=250,
+        imgsz=960,
+        batch=8,
         device=0,
         workers=2,
-        cache="ram", # "disk" or False if not enough dedicated RWAM
-
+        cache="ram",
+        save_period=50,
         patience=50,
-        optimizer="MuSGD",
-
-        momentum  = 0.937,
-        weight_decay = 0.0005,
-
+        optimizer="AdamW",
+        momentum=0.937,
+        weight_decay=0.0005,
         dropout=0.1,
         warmup_epochs=5,
-
         # ---- DATA AUGMENTATION ----
-
-        fliplr = 0.25,
-        flipud = 0.1,
-
+        fliplr=0.25,
+        flipud=0.1,
         hsv_h=0.015,
         hsv_s=0.35,
         hsv_v=0.2,
-
-        mosaic = 0.0,
-
+        mosaic=0.0,
+        auto_augment="randaugment",
+        seed=seed,
         project=r'runs'
     )
 
-
 if __name__ == "__main__":
     main()
+
+# BASE
+
+# data = r'data.yaml',
+# epochs = 350,
+# imgsz = 640,
+# batch = 8,
+# device = 0,
+# workers = 2,
+# cache = "ram",
+# save_period = 50,
+#
+# patience = 50,
+# optimizer = "AdamW",
+#
+# momentum = 0.937,
+# weight_decay = 0.0005,
+#
+# dropout = 0.1,
+# warmup_epochs = 5,
+#
+# # ---- DATA AUGMENTATION ----
+#
+# fliplr = 0.25,
+# flipud = 0.1,
+#
+# hsv_h = 0.015,
+# hsv_s = 0.35,
+# hsv_v = 0.2,
+#
+# mosaic = 0.0,
+#
+# seed = seed,
+#
+# project = r'runs'
+
+# ULTRALITICS
+#
+# data=r'data.yaml',
+# device = 0,
+# mosaic=0.5,
+# mixup=0.0,
+# copy_paste=0.0,
+# lr0=0.001,
+# epochs=50,
+# freeze=10,
+# seed=seed,
+# project=r'runs'
